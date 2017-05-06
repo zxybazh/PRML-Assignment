1b22186fe56140b516147d52950fc34b0cff4d9c
Wexley Zhou <wexleyz@nvidia.com>
2017-05-06 22:44:08 +0800

    auto-trace

--- assignment2/src/dataloader.py
+++ assignment2/src/dataloader.py
@@ -18,8 +18,9 @@ def load_data(t="z", preprocess=False, verbose=False, bias=False):
 
 	def znormalization(d):
 		print np.mean(d[:,0]), np.var(d[:,0])
+		print d[:,0]
 		d = preprocessing.normalize(d[:, 0])
-		print np.mean(d), np.var(d)
+		print np.mean(d[:,0]), np.var(d[:,0])
 		return d
 
 	def flat(d):

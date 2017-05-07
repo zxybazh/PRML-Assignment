70dc3744e79327f86e6fc0e7938e7a02d351d292
Wexley Zhou <wexleyz@nvidia.com>
2017-05-07 19:46:51 +0800

    auto-trace

--- assignment2/src/plot.py
+++ assignment2/src/plot.py
@@ -10,10 +10,10 @@ file = open("../output/Beta.out", "r")
 data1 = np.array([eval(line) for line in file.readlines()])
 
 data_alpha = [i*0.5 for i in xrange(201)]
-print data_alpha
+#print data_alpha
 
 df = pd.DataFrame(columns = ["alpha", "time", "xxx"])
-for i in xrange(4):
+for i in xrange(len(data1)):
     d = {"alpha": data_alpha[i], "time": data1[i], "xxx": "Beta-Bernoulli Naive Bayes"}
     df = df.append(pd.DataFrame(d, index = [0], columns = ["alpha", "time", "xxx"]), ignore_index=True)
 

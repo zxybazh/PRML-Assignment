a8551f162b15e993f092ffeac9ef53674f231ffe
Wexley Zhou <wexleyz@nvidia.com>
2017-05-07 19:44:50 +0800

    auto-trace

--- assignment2/src/plot.py
+++ assignment2/src/plot.py
@@ -8,17 +8,18 @@ import random
 sns.set(style="whitegrid", color_codes=True)
 file = open("../output/Beta.out", "r")
 data1 = np.array([eval(line) for line in file.readlines()])
-print data1
 
-# data_config = [1, 2, 3, 4]
-# df1 = pd.DataFrame(columns = ["config", "time", "xxx"])
-# for i in xrange(4):
-#     d = {"config": data_config[i], "time": data1[i], "xxx": "ShMem%"}
-#     df1 = df1.append(pd.DataFrame(d, index = [0], columns = ["config", "time", "xxx"]), ignore_index=True)
-# df1
+data_alpha = [i*0.5 for i in xrange(201)]
+print data_alpha
 
-# bar = sns.pointplot(x="config", y="time", hue = "xxx", data=df1, markers = "*")
+df = pd.DataFrame(columns = ["alpha", "time", "xxx"])
+for i in xrange(4):
+    d = {"alpha": data_alpha[i], "time": data1[i], "xxx": "ShMem%"}
+    df1 = df1.append(pd.DataFrame(d, index = [0], columns = ["alpha", "time", "xxx"]), ignore_index=True)
+df1
 
-# bar.legend(loc='upper right')
-# bar.set(xlabel='\alpha', ylabel='Time', title='test title')
-# plt.savefig('../output/foo.pdf')
+bar = sns.pointplot(x="alpha", y="time", hue = "xxx", data=df1, markers = "*")
+
+bar.legend(loc='upper right')
+bar.set(xlabel='\alpha', ylabel='Time', title='test title')
+plt.savefig('../output/foo.pdf')

4313c873a17370062a0930ca474d912e73eecd42
Wexley Zhou <wexleyz@nvidia.com>
2017-05-07 20:07:58 +0800

    auto-trace

--- assignment2/src/plot.py
+++ assignment2/src/plot.py
@@ -16,14 +16,16 @@ data1 = np.array([eval(line) for line in file.readlines()])
 data_alpha = [i*0.5 for i in xrange(201)]
 #print data_alpha
 
-df = pd.DataFrame(columns = ["alpha", "time", "xxx"])
+df = pd.DataFrame(columns = ["alpha", "Error rate", "xxx"])
 for i in xrange(len(data1)):
-	if (i % 10 != 0): data_alpha[i] = '';
-	else: data_alpha[i] = str(data_alpha[i])
-	d = {"alpha": data_alpha[i], "time": data1[i], "xxx": "Beta-Bernoulli Naive Bayes"}
-    df = df.append(pd.DataFrame(d, index = [0], columns = ["alpha", "time", "xxx"]), ignore_index=True)
+	if (i % 10 != 0):
+		data_alpha[i] = '';
+	else:
+		data_alpha[i] = str(data_alpha[i])
+	d = {"alpha": data_alpha[i], "Error rate": data1[i], "xxx": "Beta-Bernoulli Naive Bayes"}
+    df = df.append(pd.DataFrame(d, index = [0], columns = ["alpha", "Error rate", "xxx"]), ignore_index=True)
 
-bar = sns.pointplot(x="alpha", y="time", hue = "xxx", data=df)
+bar = sns.pointplot(x="alpha", y="Error rate", hue = "xxx", data=df)
 
 bar.legend(loc='upper right')
 bar.set(xlabel='$\\alpha$', ylabel='Time', title='Beta-Bernoulli Naive Bayes Model Error Rate Change with $\\alpha$ value')

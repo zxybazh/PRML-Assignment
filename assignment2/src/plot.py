825a57b2f723bb9d9f5553679086a7d867ff53ec
Wexley Zhou <wexleyz@nvidia.com>
2017-05-07 19:58:55 +0800

    auto-trace

--- assignment2/src/plot.py
+++ assignment2/src/plot.py
@@ -19,7 +19,8 @@ data_alpha = [i*0.5 for i in xrange(201)]
 df = pd.DataFrame(columns = ["alpha", "time", "xxx"])
 for i in xrange(len(data1)):
 	if (i % 10 != 0): data_alpha[i] = '';
-    d = {"alpha": data_alpha[i], "time": data1[i], "xxx": "Beta-Bernoulli Naive Bayes"}
+	else: data_alpha[i] = str(data_alpha[i])
+	d = {"alpha": data_alpha[i], "time": data1[i], "xxx": "Beta-Bernoulli Naive Bayes"}
     df = df.append(pd.DataFrame(d, index = [0], columns = ["alpha", "time", "xxx"]), ignore_index=True)
 
 bar = sns.pointplot(x="alpha", y="time", hue = "xxx", data=df)

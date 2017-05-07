3a60af2006bc7f307878be5a4dcd15e99b26f2b6
Wexley Zhou <wexleyz@nvidia.com>
2017-05-07 15:59:33 +0800

    auto-trace

--- assignment2/src/naive_bayes_classifier.py
+++ assignment2/src/naive_bayes_classifier.py
@@ -74,11 +74,12 @@ class BetaNaiveBayesClassifier(GenerativeClassifier):
                 self.count[0] += 1
                 self.ecount[y] += 1
         print
-        print "="*20, "Beta Naive Bayes Classifier", "="*20
+        print "-" * 10, "Beta Naive Bayes Classifier", "-" * 10
         print "Correct Classcification:", self.count[1], ", Wrong Classcification:", self.count[0]
         print "Spam => Normal:", self.ecount[0], ", Normal => Spam:", self.ecount[1]
         ratio = 100 * self.count[1] / float(len(self.y_test))
         print "Correct Ratio: ", ratio, "%"
+        print "-" * 49
         return ratio
 
 class GaussianNaiveBayesClassifier(GenerativeClassifier):

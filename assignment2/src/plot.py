2f5fb9d63fee6bc68560b7efb706e76a00cf4972
Wexley Zhou <wexleyz@nvidia.com>
2017-05-07 18:28:26 +0800

    auto-trace

--- assignment2/src/plot.py
+++ assignment2/src/plot.py
@@ -6,9 +6,7 @@ from matplotlib import style
 import random
 
 sns.set(style="whitegrid", color_codes=True)
-plt.figure(figsize=(15, 9))
 
-norm = 7.0
 data1 = np.array([11, 12, 13, 14])
 data_config = [1, 2, 3, 4]
 df1 = pd.DataFrame(columns = ["config", "time", "xxx"])
@@ -18,11 +16,7 @@ for i in xrange(4):
 df1
 
 bar = sns.pointplot(x="config", y="time", hue = "xxx", data=df1, markers = "*")
-for p in zip(bar.get_xticks(), df1["time"]):
-    bar.text(p[0]-0.1, p[1]+0.2, str(p[1]*norm)+"%", color='#FFA500', fontsize=15)
 
 bar.legend(loc='upper right')
-
-bar.set(xlabel='$\alpha$', ylabel='Time', title='test title')
-#plt.show()
+bar.set(xlabel='\alpha', ylabel='Time', title='test title')
 plt.savefig('../output/foo.pdf')

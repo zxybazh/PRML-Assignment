34043f4966baeae42198084ab70db1711795e941
Wexley Zhou <wexleyz@nvidia.com>
2017-05-07 20:17:01 +0800

    auto-trace

--- assignment2/src/plot.py
+++ assignment2/src/plot.py
@@ -7,7 +7,7 @@ import random
 
 
 sns.set(style="whitegrid", color_codes=True)
-paper_rc = {'lines.linewidth': 0.4, 'lines.markersize': 0.8}
+paper_rc = {'lines.linewidth': 0.8, 'lines.markersize': 1.2}
 sns.set_context("paper", rc = paper_rc)   
 
 file = open("../output/Beta.out", "r")
@@ -22,7 +22,7 @@ for i in xrange(len(data1)):
 	d = {"alpha": data_alpha[i], "Error rate": data1[i]}
 	df = df.append(pd.DataFrame(d, index = [0], columns = ["alpha", "Error rate"]), ignore_index=True)
 
-bar = sns.regplot(x="alpha", y="Error rate", data=df)
+bar = sns.regplot(x="alpha", y="Error rate", data=df, color = 'g')
 
 bar.set(xlabel='$\\alpha$', ylabel='Time', title='Beta-Bernoulli Naive Bayes Model Error Rate Change with $\\alpha$ value')
 

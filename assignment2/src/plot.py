b46c0b9ce861a1b70c4f7360b1e59b15116841bd
Wexley Zhou <wexleyz@nvidia.com>
2017-05-07 19:55:54 +0800

    auto-trace

--- assignment2/src/plot.py
+++ assignment2/src/plot.py
@@ -7,7 +7,7 @@ import random
 
 
 sns.set(style="whitegrid", color_codes=True)
-paper_rc = {'lines.linewidth': 1, 'lines.markersize': 3}
+paper_rc = {'lines.linewidth': 0.4, 'lines.markersize': 0.8}
 sns.set_context("paper", rc = paper_rc)   
 
 file = open("../output/Beta.out", "r")
@@ -17,8 +17,8 @@ data_alpha = [i*0.5 for i in xrange(201)]
 #print data_alpha
 
 df = pd.DataFrame(columns = ["alpha", "time", "xxx"])
-for i in xrange(len(data1) / 5):
-    d = {"alpha": data_alpha[i*5], "time": data1[i*5], "xxx": "Beta-Bernoulli Naive Bayes"}
+for i in xrange(len(data1)):
+    d = {"alpha": data_alpha[i], "time": data1[i], "xxx": "Beta-Bernoulli Naive Bayes"}
     df = df.append(pd.DataFrame(d, index = [0], columns = ["alpha", "time", "xxx"]), ignore_index=True)
 
 bar = sns.pointplot(x="alpha", y="time", hue = "xxx", data=df, markers = '*')

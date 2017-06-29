530b1e297378e3689ad9f04d2ae0685e23b7bff9
Wexley Zhou <wexleyz@nvidia.com>
2017-06-29 23:06:55 +0800

    auto-trace

--- assignment3/src/discriminative_classfier.py
+++ assignment3/src/discriminative_classfier.py
@@ -70,8 +70,8 @@ class LogisticRegression(DiscriminativeClassifier):
         err = 0
         for i in xrange(len(self.x_test)):
         	y_1 = sigmoid(self.weight * np.append(self.x_test, 1))
-        	y_0 = 1 - y_1
-        	err -= self.y_test[i]*math.log(y_1) + (1-self.y_test[i])*math.log(y_0)
+        	err -= self.y_test[i]*math.log(y_1) + (1-self.y_test[i])*math.log(1-y_1)
+        for para in xrange
         return err
 
 

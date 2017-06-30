72a145b2583874e947f04ec00e803e7d802527b5
Wexley Zhou <wexleyz@nvidia.com>
2017-06-30 21:54:43 +0800

    auto-trace

--- assignment3/src/plot.py
+++ assignment3/src/plot.py
@@ -7,29 +7,29 @@ import pandas as pd
 from matplotlib import style
 import random
 
-e_training = []
-e_test = []
+# e_training = []
+# e_test = []
 
-for Lambda in range(1, 10) + range(10, 105, 5):
-	print "Processed to", Lambda
-	logistic = LogisticRegression(l2norm=Lambda);
-	acc_p, err_p = logistic.train()
-	acc_q, err_q = logistic.test()
-	e_training.append(acc_p)
-	e_test.append(acc_q)
+# for Lambda in range(1, 10) + range(10, 105, 5):
+# 	print "Processed to", Lambda
+# 	logistic = LogisticRegression(l2norm=Lambda);
+# 	acc_p, err_p = logistic.train()
+# 	acc_q, err_q = logistic.test()
+# 	e_training.append(acc_p)
+# 	e_test.append(acc_q)
 
-file = open("../output/plot_training.out", "w")
-print >> file, e_training
-file.close()
+# file = open("../output/plot_training.out", "w")
+# print >> file, e_training
+# file.close()
 
-file1 = open("../output/plot_test.out", "w")
-print >> file1, e_test
-file1.close()
+# file1 = open("../output/plot_test.out", "w")
+# print >> file1, e_test
+# file1.close()
 
-# file = open("../output/plot_training.out", "r")
-# e_training = np.array([eval(line) for line in file.readlines()])
-# file1 = open("../output/plot_test.out", "r")
-# e_test = np.array([eval(line) for line in file1.readlines()])
+file = open("../output/plot_training.out", "r")
+e_training = np.array([eval(line) for line in file.readlines()])
+file1 = open("../output/plot_test.out", "r")
+e_test = np.array([eval(line) for line in file1.readlines()])
 
 data_lambda = range(1, 10) + range(10, 105, 5)
 
@@ -41,10 +41,10 @@ df = pd.DataFrame(columns = ["lambda", "Error rate", "Error type"])
 
 for i in xrange(len(data_lambda)):
 	d = {"Lambda": data_lambda[i], "Training Error": e_training[i], "Error Type": "Training Error"}
-	d = {"Lambda": data_lambda[i], "Error Rate": e_training[i], "Error Type": "Training Error"}
+	d = {"Lambda": data_lambda[i], "Error Rate": e_training[i], "Error Type": "Test Error"}
 	df = df.append(pd.DataFrame(d, index = [0], columns = ["Lambda", "Error rate", "Error type"]), ignore_index=True)
 
-bar = sns.regplot(x="lambda", y="Error rate", data=df, color = 'g')
+bar = sns.regplot(x="Lambda", y="Error rate", hue = "Error Type", data=df, color = 'g')
 
 bar.set(xlabel='$\\lambda$', ylabel='Error rate', title='Logistic Regressor Error Rate Change with $\\lambda$ value after 30 epochs')
 

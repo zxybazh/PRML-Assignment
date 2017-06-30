13003d57d4e833d0bf1723b41754be1a1cd61ee2
Wexley Zhou <wexleyz@nvidia.com>
2017-07-01 00:10:18 +0800

    auto-trace

--- assignment3/src/discriminative_classfier.py
+++ assignment3/src/discriminative_classfier.py
@@ -206,7 +206,7 @@ class LinearRegression(DiscriminativeClassifier):
 		for i in xrange(len(self.x_test)):
 			# P(y = 1) = \sigmoid ( W ^ T * X)
 			# P(y = 0) = 1 - P(y = 1)
-			y_1 = sigmoid(self.weight.dot(np.insert(self.x_test[i], 0, 1)))
+			y_1 = self.weight.dot(np.insert(self.x_test[i], 0, 1))
 			y_0 = 1-y_1
 			# J(\theta) = - \sum{ y_i * \log(h_theta{x_i}) + (1-y_i) * \log(1-h_theta{x_i}) }
 			err -= self.y_test[i]*math.log(y_1+1e-9) + (1-self.y_test[i])*math.log(y_0+1e-9)

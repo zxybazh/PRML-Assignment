c8155a64d2ce789da6ed3813ff0ff91693224bc5
Wexley Zhou <wexleyz@nvidia.com>
2017-06-30 21:51:42 +0800

    auto-trace

--- assignment3/src/plot.py
+++ assignment3/src/plot.py
@@ -18,6 +18,19 @@ for Lambda in range(1, 10) + range(10, 105, 5):
 	e_training.append(acc_p)
 	e_test.append(acc_q)
 
+file = open("../output/plot_training.out", "w")
+print >> file, e_training
+file.close()
+
+file1 = open("../output/plot_test.out", "w")
+print >> file1, e_test
+file1.close()
+
+# file = open("../output/plot_training.out", "r")
+# e_training = np.array([eval(line) for line in file.readlines()])
+# file1 = open("../output/plot_test.out", "r")
+# e_test = np.array([eval(line) for line in file1.readlines()])
+
 data_lambda = range(1, 10) + range(10, 105, 5)
 
 sns.set(style="darkgrid", color_codes=True)

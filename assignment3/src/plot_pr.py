162bc16db357eccc8083d1f2abefe9b28b2deb43
Wexley Zhou <wexleyz@nvidia.com>
2017-07-01 04:54:30 +0800

    auto-trace

--- assignment3/src/plot_pr.py
+++ assignment3/src/plot_pr.py
@@ -8,20 +8,18 @@ from matplotlib import style
 import random
 
 
-file = open("../output/linear_plot_training.out", "r")
+file = open("../output/sgd.out", "r")
 lines = file.readlines()
-Recall_training    = np.array([eval(line) for line in file.readlines()])
-Precision_training = np.array([eval(line) for line in file.readlines()])[
-Recall_test        = np.array([eval(line) for line in file.readlines()])[0]
-Precision_test     = np.array([eval(line) for line in file.readlines()])[0]
-# file1 = open("../output/linear_plot_test.out", "r")
-# e_test = np.array([eval(line) for line in file1.readlines()])[0]
+Recall_training    = np.array(eval(lines[0]))
+Precision_training = np.array(eval(lines[1]))
+Recall_test        = np.array(eval(lines[2]))
+Precision_test     = np.array(eval(lines[3]))
 
 sns.set(style="darkgrid", color_codes=True)
 paper_rc = {'lines.linewidth': 1, 'lines.markersize': 1.2}
 #sns.set_context("paper", rc = paper_rc)
 
-df = pd.DataFrame(columns = ["Lambda", "Error Rate", "Type"])
+df = pd.DataFrame(columns = ["Recall", "Precision", "Type"])
 
 
 for i in xrange(len(data_lambda)):

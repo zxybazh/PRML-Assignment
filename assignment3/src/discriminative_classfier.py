e5921a218bf1eb7b7196058c19b0a38fd0b09c3d
Wexley Zhou <wexleyz@nvidia.com>
2017-07-01 01:50:44 +0800

    auto-trace

--- assignment3/src/discriminative_classfier.py
+++ assignment3/src/discriminative_classfier.py
@@ -215,7 +215,7 @@ class LinearRegression(DiscriminativeClassifier):
 		self.ecount = [0, 0]
 		# Error rate
 		ratio = -1
-		print np.asarray(self.weight).shape
+		self.weight = np.array([np.float(w) for w in self.weight])
 		err = norm(self.weight, np.c_[np.ones(len(self.x_test)), self.x_test].T - self.y_test)
 		for i in xrange(len(self.x_test)):
 			y_1 = self.weight.dot(np.insert(self.x_test[i], 0, 1))

a84e7ef63d5838a9d88afe5b88d91e176a1f5725
Wexley Zhou <wexleyz@nvidia.com>
2017-06-30 21:32:38 +0800

    auto-trace

--- assignment3/src/plot.py
+++ assignment3/src/plot.py
@@ -1,6 +1,7 @@
 from dataloader import *
 from discriminative_classifier import *
 
-	logistic = LogisticRegression(l2norm=1);
+for Lambda in range(1, 10) + range(10, 105, 5):
+	logistic = LogisticRegression(l2norm=Lambda);
 	acc, err = logistic.train()
 	acc, err = logistic.test()

a4b535589a652540b6f3d228661ada5e0114a0cc
Wexley Zhou <wexleyz@nvidia.com>
2017-07-01 00:44:26 +0800

    auto-trace

--- assignment3/src/discriminative_classfier.py
+++ assignment3/src/discriminative_classfier.py
@@ -31,6 +31,12 @@ def calc_grad(W, X, Y):
 	return grad, Hess
 
 
+def linear_least_squares(a, b):
+    a = np.asarray(a, order='c')
+    i = dgemm(alpha=1.0, a=a.T, b=a.T, trans_b=True)
+    x = np.linalg.solve(i, dgemm(alpha=1.0, a=a.T, b=b)).flatten()
+    return x, np.linalg.norm(np.dot(a, x) - b)
+
 class LogisticRegression(DiscriminativeClassifier):
 
 	def __init__(self, l2norm=1, preprocessing="z", eta=1e-4, max_epoch=30, l2_on=True):

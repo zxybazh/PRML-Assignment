edc697e4574e2493bd2c3c9cca656500604217d8
Wexley Zhou <wexleyz@nvidia.com>
2017-06-30 20:50:26 +0800

    auto-trace

--- assignment3/src/discriminative_classfier.py
+++ assignment3/src/discriminative_classfier.py
@@ -57,28 +57,32 @@ class LogisticRegression(DiscriminativeClassifier):
 		# Count Error Type & Number
 		self.ecount = [0, 0]
 		while True:
-			update = calc_grad(self.weight, self.x_train, self.y_train)
 			# update weight
+			update = calc_grad(self.weight, self.x_train, self.y_train)
 			self.weight = self.weight + self.eta*update[0]
+
 			# if norm(update) < self.eta * 0.1: # TODO: you should think about some early stopping scheme here
 			#	break
+
 			err = 0
 			for i in xrange(len(self.x_train)):
 				y_1 = sigmoid(self.weight.dot(np.insert(self.x_train[i], 0, 1)))
 				y_0 = 1-y_1
 				if (y_0 > y_1): y = 0
 				else: y = 1
-				if y == self.y_test[i]: self.count[1]  += 1
+				if y == self.y_train[i]: self.count[1]  += 1
 				else: self.count[0]  += 1
 				err -= self.y_train[i]*math.log(y_1+eps) + (1-self.y_train[i])*math.log(y_0+eps)
 			if self.l2_on:
 				for para in self.weight:
 					err += l2norm/2.0*para*para
+
 			epoch += 1
 			print "epoch\t", epoch, "\ttraining loss:", err
+			ratio = 100 * self.count[0] / float(len(self.y_train))
 			print "Training Error Ratio: ", ratio, "%"
 			print "-" * 59
-			return err
+
 			if epoch > self.max_epoch:
 				break
 

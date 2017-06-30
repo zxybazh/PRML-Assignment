1eead96f4f13096aa5c5a03f71c07671c03e2543
Wexley Zhou <wexleyz@nvidia.com>
2017-06-30 21:34:38 +0800

    auto-trace

--- assignment3/src/plot.py
+++ assignment3/src/plot.py
@@ -7,39 +7,33 @@ import pandas as pd
 from matplotlib import style
 import random
 
-sns.set(style="darkgrid", color_codes=True)
-paper_rc = {'lines.linewidth': 0.8, 'lines.markersize': 1.2}
-sns.set_context("paper", rc = paper_rc)  
-
 for Lambda in range(1, 10) + range(10, 105, 5):
 	logistic = LogisticRegression(l2norm=Lambda);
 	acc_p, err_p = logistic.train()
 	acc_q, err_q = logistic.test()
-	
-	file = open("../output/Beta.out", "r")
-	data1 = np.array([eval(line) for line in file.readlines()])
-	file1 = open("../output/Beta_train.out", "r")
-	data2 = np.array([eval(line) for line in file1.readlines()])
 
-	data_alpha = [i*0.5 for i in xrange(201)]
-	#print data_alpha
+data_lambda = range(1, 10) + range(10, 105, 5)
 
-	df = pd.DataFrame(columns = ["alpha", "Error rate"])
+sns.set(style="darkgrid", color_codes=True)
+paper_rc = {'lines.linewidth': 0.8, 'lines.markersize': 1.2}
+sns.set_context("paper", rc = paper_rc)
 
-	for i in xrange(len(data1)):
-		d = {"alpha": data_alpha[i], "Error rate": data1[i]}
-		df = df.append(pd.DataFrame(d, index = [0], columns = ["alpha", "Error rate"]), ignore_index=True)
+df = pd.DataFrame(columns = ["alpha", "Error rate"])
 
-	bar = sns.regplot(x="alpha", y="Error rate", data=df, color = 'g')
+for i in xrange(len(data1)):
+	d = {"alpha": data_alpha[i], "Error rate": data1[i]}
+	df = df.append(pd.DataFrame(d, index = [0], columns = ["alpha", "Error rate"]), ignore_index=True)
 
-	df = pd.DataFrame(columns = ["alpha", "Error rate"])
+bar = sns.regplot(x="alpha", y="Error rate", data=df, color = 'g')
 
-	for i in xrange(len(data1)):
-		d = {"alpha": data_alpha[i], "Error rate": data2[i]}
-		df = df.append(pd.DataFrame(d, index = [0], columns = ["alpha", "Error rate"]), ignore_index=True)
+df = pd.DataFrame(columns = ["alpha", "Error rate"])
 
-	bar = sns.regplot(x="alpha", y="Error rate", data=df, color = 'r')
+for i in xrange(len(data1)):
+	d = {"alpha": data_alpha[i], "Error rate": data2[i]}
+	df = df.append(pd.DataFrame(d, index = [0], columns = ["alpha", "Error rate"]), ignore_index=True)
 
-	bar.set(xlabel='$\\alpha$', ylabel='Error rate', title='Beta-Bernoulli Naive Bayes Model Error Rate Change with $\\alpha$ value')
+bar = sns.regplot(x="alpha", y="Error rate", data=df, color = 'r')
 
-	plt.savefig('../output/foo.pdf')
+bar.set(xlabel='$\\alpha$', ylabel='Error rate', title='Beta-Bernoulli Naive Bayes Model Error Rate Change with $\\alpha$ value')
+
+plt.savefig('../output/foo.pdf')

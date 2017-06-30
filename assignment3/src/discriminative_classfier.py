2692abd58b3e373c8af79e73326292bf4d1c5524
Wexley Zhou <wexleyz@nvidia.com>
2017-07-01 04:26:23 +0800

    auto-trace

--- assignment3/src/discriminative_classfier.py
+++ assignment3/src/discriminative_classfier.py
@@ -180,18 +180,18 @@ class LinearRegression(DiscriminativeClassifier):
 
 			epoch = 0
 			# Error rate
-			Recall    = []
-			Precision = []
+			Recall_training    = []
+			Precision_training = []
 			while True:
 				momentum = 0
 				for i in xrange(len(self.x_train), self.miniBatch):
 					
 					momentum = momentum * self.gamma + self.eta * update
-					
 					if self.l2_on:
 						momentum += self.eta * self.L2norm * np.insert(self.weight[1:], 0, 0);
+
 					self.weight -= momentum
-					err = 0
+
 					for i in xrange(len(self.x_train)):
 						y_1 = sigmoid(self.weight.dot(np.insert(self.x_train[i], 0, 1)))
 						y_0 = 1-y_1

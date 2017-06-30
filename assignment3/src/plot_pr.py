59a68a7c716c565ec2bec7a9b8b184634676e508
Wexley Zhou <wexleyz@nvidia.com>
2017-07-01 05:22:37 +0800

    auto-trace

--- assignment3/src/plot_pr.py
+++ assignment3/src/plot_pr.py
@@ -7,29 +7,30 @@ import pandas as pd
 from matplotlib import style
 import random
 
-linear = LinearRegression(miniBatch = 256, method = "sgd")
-eve = linear.train()
-Precision_training = np.array(eve[0])
-Recall_training    = np.array(eve[1])
-Precision_test     = np.array(eve[2])
-Recall_test        = np.array(eve[3])
+for eta in [1e-5, 1e-4, 1e-3, 1e-2]:
+	linear = LinearRegression(miniBatch = 256, method = "sgd", eta = eta)
+	eve = linear.train()
+	Precision_training = np.array(eve[0])
+	Recall_training    = np.array(eve[1])
+	Precision_test     = np.array(eve[2])
+	Recall_test        = np.array(eve[3])
 
-sns.set(style="darkgrid", color_codes=True)
-paper_rc = {'lines.linewidth': 1, 'lines.markersize': 1.2}
-#sns.set_context("paper", rc = paper_rc)
+	sns.set(style="darkgrid", color_codes=True)
+	paper_rc = {'lines.linewidth': 1, 'lines.markersize': 1.2}
+	#sns.set_context("paper", rc = paper_rc)
 
-df = pd.DataFrame(columns = ["Recall", "Precision", "Type"])
+	df = pd.DataFrame(columns = ["Recall", "Precision", "Type"])
 
-for i in xrange(len(Recall_training)):
-	d  = {"Recall": Recall_training[i], "Precision": Precision_training[i], "Type": "Training"}
-	df = df.append(pd.DataFrame(d, index = [0], columns = ["Lambda", "Error Rate", "Type"]), ignore_index=True)
+	for i in xrange(len(Recall_training)):
+		d  = {"Recall": Recall_training[i], "Precision": Precision_training[i], "Type": "Training"}
+		df = df.append(pd.DataFrame(d, index = [0], columns = ["Lambda", "Error Rate", "Type"]), ignore_index=True)
 
-for i in xrange(len(Recall_test)):
-	d  = {"Recall": Recall_test[i], "Precision": Precision_test[i], "Type": "Test"}
-	df = df.append(pd.DataFrame(d, index = [0], columns = ["Lambda", "Error Rate", "Type"]), ignore_index=True)
+	for i in xrange(len(Recall_test)):
+		d  = {"Recall": Recall_test[i], "Precision": Precision_test[i], "Type": "Test"}
+		df = df.append(pd.DataFrame(d, index = [0], columns = ["Lambda", "Error Rate", "Type"]), ignore_index=True)
 
-plt.plot(Recall_training, Precision_training, label='Precision-Recall curve')
-plt.show()
+	plt.plot(Recall_training, Precision_training, label='Precision-Recall curve')
+	plt.show()
 
-plt.plot(Recall_test, Precision_test, label='Precision-Recall curve')
-plt.show()
+	plt.plot(Recall_test, Precision_test, label='Precision-Recall curve')
+	plt.show()

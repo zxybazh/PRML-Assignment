a5fdf3a3ab3037187b03ed98d9a621beecc9f5ba
Wexley Zhou <wexleyz@nvidia.com>
2017-07-01 01:23:36 +0800

    auto-trace

--- assignment3/src/discriminative_classfier.py
+++ assignment3/src/discriminative_classfier.py
@@ -134,7 +134,7 @@ class LogisticRegression(DiscriminativeClassifier):
 
 class LinearRegression(DiscriminativeClassifier):
 
-	def __init__(self, l2norm=1, preprocessing="z", eta=1e-4, max_epoch=30, l2_on=False):
+	def __init__(self, l2norm=1, preprocessing="z", eta=1e-4, max_epoch=30, l2_on=False, method="lsq"):
 		"""
 		:param l2norm: l2 norm penalty
 		:param preprocessing: preprocessing method
@@ -148,8 +148,10 @@ class LinearRegression(DiscriminativeClassifier):
 		self.eta = eta
 		self.max_epoch = max_epoch
 		self.l2_on = l2_on
+		self.method = method
 
 	def train(self, eps=1e-4):
+
 		res, err = np.linalg.lstsq(self.x_train, self.y_train)[:2]
 		print res
 		print err

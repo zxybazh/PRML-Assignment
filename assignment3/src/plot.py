21c07444ae3174815d4ecb91cf01f20abad96631
Wexley Zhou <wexleyz@nvidia.com>
2017-06-30 21:35:39 +0800

    auto-trace

--- assignment3/src/plot.py
+++ assignment3/src/plot.py
@@ -7,10 +7,15 @@ import pandas as pd
 from matplotlib import style
 import random
 
+e_training = []
+e_test = []
+
 for Lambda in range(1, 10) + range(10, 105, 5):
 	logistic = LogisticRegression(l2norm=Lambda);
 	acc_p, err_p = logistic.train()
 	acc_q, err_q = logistic.test()
+	e_training.append(acc_p)
+	e_test.append(acc_q)
 
 data_lambda = range(1, 10) + range(10, 105, 5)
 
@@ -18,7 +23,7 @@ sns.set(style="darkgrid", color_codes=True)
 paper_rc = {'lines.linewidth': 0.8, 'lines.markersize': 1.2}
 sns.set_context("paper", rc = paper_rc)
 
-df = pd.DataFrame(columns = ["alpha", "Error rate"])
+df = pd.DataFrame(columns = ["lambda", "Training Error", "Test Error"])
 
 for i in xrange(len(data1)):
 	d = {"alpha": data_alpha[i], "Error rate": data1[i]}
